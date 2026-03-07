963957bc6879b1b4857b9c578230a6e8bef327c1
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-03-07 16:46:24 +0545

    Refactor schedule model

--- src/modules/schedule/model.js
+++ src/modules/schedule/model.js
@@ -12,28 +12,47 @@ const daysOfWeek = [
 
 const timeRegex = /^([01]\d|2[0-3]):([0-5]\d)$/;
 
+const timeTableEntrySchema = new mongoose.Schema(
+  {
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    day: {
+      type: String,
+      enum: daysOfWeek,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      match: timeRegex,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      match: timeRegex,
+      required: true,
+    },
+    room: {
+      type: String,
+      default: "TBD",
+    },
+  },
+  { _id: true },
+);
+
 const scheduleSchema = new mongoose.Schema(
   {
     class: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Class",
       required: true,
-      index: true,
       unique: true,
+      index: true,
     },
-    timeTable: [
-      {
-        course: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Course",
-          required: true,
-        },
-        day: { type: String, enum: daysOfWeek, required: true },
-        startTime: { type: String, match: timeRegex, required: true },
-        endTime: { type: String, match: timeRegex, required: true },
-        room: { type: String, default: "TBD" },
-      },
-    ],
+
+    timeTable: [timeTableEntrySchema],
   },
   { timestamps: true },
 );
