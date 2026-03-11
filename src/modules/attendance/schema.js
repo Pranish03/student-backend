335ff28c5505f61dded8a513047928e713acddc7
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-03-11 17:08:47 +0545

    Finish attendance schema

---
+++ src/modules/attendance/schema.js
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const objectID = z
+  .string()
+  .regex(/^[0-9a-fA-F]{24}$/, "Invalid ObjectId");
+
+const attendance = z.object({
+  student: objectID,
+  isPresent: z.boolean.default(false),
+});
+
+export const createAttendanceSchema = z.object({
+  course: objectID,
+  date: z.date(),
+  attendance: z.array(attendance),
+});
+
+export const editAttendanceSchema = z
+  .object({
+    attendance: z.array(attendance),
+  })
+  .partial();
